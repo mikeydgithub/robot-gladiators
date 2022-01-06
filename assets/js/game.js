e8ef650f4105af457b849b360f593dd0f43158bf
Michael Diamond <94988620+mikeydgithub@users.noreply.github.com>
2022-01-06 15:19:19 -0500

    added checking player and enemy's health

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,6 +21,14 @@ var fight = function() {
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
 
+    // check player's health
+    if (playerHealth <=0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.")
+    }    
+
     //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in 'playerHealth' variable.
     
     //Log a resulting message to the console so we know that it worked.
@@ -28,5 +36,13 @@ var fight = function() {
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     )
 };
+
+//check enemy's health
+if (enemyHealth <=0) {
+    window.alert(enemyName + " has died!");
+}
+else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+}
 
 fight();
